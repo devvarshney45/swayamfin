eb5573b10adfebf1ca2233d09be6bf0911f2699f
Dev Varshney <varshneydev365@gmail.com>
2026-05-01 20:22:54 +0530

    hello

--- backend/routes/branchRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getBranches, createBranch } = require('../controllers/branchController');
-const { protect, adminOnly } = require('../middleware/authMiddleware');
-
-router.get('/', protect, getBranches); // Agents might need it for dropdowns
-router.post('/', protect, adminOnly, createBranch);
-
-module.exports = router;
